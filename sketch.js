74c412ce7e98a7034884e90fb11b72fc2dec7c8d
Harris245 <88297534+Harris245@users.noreply.github.com>
2021-08-01 21:39:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  
+}
+
+function draw() 
+{
+  background(30);
+  fill ("red")
+  box = createSprite(200,200,20,20)
+drawSprites();
+}
+
+
+
+
